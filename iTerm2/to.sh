c08ddc96b029db5b24881710066788be931d663e
Miles.Mai <miles.mai@corp.globalmarket.com>
2015-07-07 15:05:01 +0800

    add sta-ssh

--- iTerm2/to.sh
+++ iTerm2/to.sh
@@ -3,6 +3,10 @@ cat<<MENU
 1)MVO-86.52         2)soa_core-86.121        3)soa_order-172.31.80.254
 4)SOA-86.121        5)like-86.65             6)hudson_86.23
 
+= = = = = = = = = = = = = = = sta = = = = = = = = = = = = = = =
+1)docker-86.227
+
+
 = = = = = = = = = = = = = = = prd = = = = = = = = = = = = = = =
 1)MVO-24.80         2)MVO-24.81              3)MVO-24.82
 4)SOA-24.150        5)SOA-24.151
@@ -47,6 +51,12 @@ case "$env" in
 			7) exec ssh deploy@192.168.24.94
 			;;
 			8) exec ssh gmbatch@192.168.24.119
+			;;
+		esac
+	;;
+	sta)
+		case "$num" in
+			1) exec ssh gmuser@192.168.86.227
 			;;
 		esac
 	;;
